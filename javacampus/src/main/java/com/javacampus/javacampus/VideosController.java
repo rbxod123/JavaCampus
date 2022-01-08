3c45dae366ba3983b55747e44d516c743087febb
kyu <rbxod123@naver.com>
2022-01-08 17:43:30 +0900

    VideosController Add

---
+++ javacampus/src/main/java/com/javacampus/javacampus/VideosController.java
@@ -0,0 +1,14 @@
+package com.javacampus.javacampus;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideosController {
+    
+    @RequestMapping(value = "/videos")
+    public String index() {
+        return "강좌 리스트";
+    }
+
+}
