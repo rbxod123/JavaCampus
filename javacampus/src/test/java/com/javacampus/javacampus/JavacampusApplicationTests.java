7c9cc588419a09ff40a9c91ed24460252a1c40e3
kyu <rbxod123@naver.com>
2022-01-08 17:09:32 +0900

    JavaCampus with spring boot first commit

---
+++ javacampus/src/test/java/com/javacampus/javacampus/JavacampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacampus.javacampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
